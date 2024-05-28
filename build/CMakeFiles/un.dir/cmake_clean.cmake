5c45c06bef2ca81f2fcdfe1f8d5525da5aea9dc9
Paulo-Camacho <anthropologyparliament@gmail.com>
2024-05-27 19:45:36 -0700

    working tree

---
+++ build/CMakeFiles/un.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/un.dir/main.cpp.o"
+  "CMakeFiles/un.dir/main.cpp.o.d"
+  "un"
+  "un.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/un.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/un.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for un.
+# This may be replaced when dependencies are built.
